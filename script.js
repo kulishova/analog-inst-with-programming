bfb9c67abe3b9597d29f160b93bef4a3f4d053eb
Dari <pinobobino@gmail.com>
2022-10-19 17:39:08 +0300

    variant

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
 // likes
 {
-    const likes = document.querySelectorAll('.post__actions .post__action:first-child')
-    console.log(likes)
-    
-    likes.forEach((item, idx) => {
-        item.addEventListener('click', () => {
-            if (item.getAttribute('like') == 'true') {
-                item.removeAttribute('like')
-                item.innerHTML = '<i class="inst_like"></i>'
-            } else {
-                item.setAttribute('like', 'true')
-                item.innerHTML = '<i class="inst_like_fill" style="color: red"></i>'
-            }
-        })
-    })
+  const postsBlock = document.querySelector('.posts')
+
+  postsBlock.addEventListener('click', (event) => {
+    const target = event.target
+
+    if (target.contains('.post__action i.inst-like')
+})
+  console.log(postsBlock)
+
+  item.addEventListener('click', () => {
+    if (item.getAttribute('like') == 'true') {
+      item.removeAttribute('like')
+      item.innerHTML = '<i class="inst_like"></i>'
+    } else {
+      item.setAttribute('like', 'true')
+      item.innerHTML = '<i class="inst_like_fill" style="color: red"></i>'
+    }
+  })
 }
 
 // Posts render
 const defaultPostData = {
-    img: 'https://placeimg.com/378/570/any?img=',
-    user: {
-        photo: 'https://i.pravatar.cc/240?img=',
-        nickname: '@user_profil'
-    }
+  img: 'https://placeimg.com/378/570/any?img=',
+  user: {
+    photo: 'https://i.pravatar.cc/240?img=',
+    nickname: '@user_profil',
+  },
 }
-
 
 // Функция генерации постов
 function postGeneration(postCount = 1) {
-    const posts = []
-    let isHorizontal = false
-    
-    function postHTMLGenerate(postImg, userPhoto, isHorizontal) {
- // Создаем блок div с классом Post 
- const post = document.createElement('div')
- post.className = isHorizontal ? 'post post_horizontal' : 'post' 
- post.innerHTML = `
+  const posts = []
+  let isHorizontal = false
+
+  function postHTMLGenerate(postImg, userPhoto, isHorizontal) {
+    // Создаем блок div с классом Post
+    const post = document.createElement('div')
+    post.className = isHorizontal ? 'post post_horizontal' : 'post'
+    post.innerHTML = `
  <div class="post__img">
  <img src="${postImg}" alt="">
  </div>
@@ -62,31 +65,30 @@ function postGeneration(postCount = 1) {
  </div>
 </div>
  `
- return post
-    }
+    return post
+  }
 
-    for (let i=1; i <= postCount / 2; i++) {
-       
-        // Создаем пост 1
-        const postImg = defaultPostData.img+i
-        const userPhoto = defaultPostData.user.photo+i
-        const post = postHTMLGenerate(postImg, userPhoto, isHorizontal)
-        
-        //  Создаем пост 2 
-        const post2Img = defaultPostData.img+i+1
-        const user2Photo = defaultPostData.user.photo+i+1
-        const post2 = postHTMLGenerate(post2Img, user2Photo, !isHorizontal)
-        
-        // Создаем блок колонку постов
-        const postsColumn = document.createElement('div') 
-        postsColumn.appendChild(post)
-        postsColumn.appendChild(post2)
+  for (let i = 1; i <= postCount / 2; i++) {
+    // Создаем пост 1
+    const postImg = defaultPostData.img + i
+    const userPhoto = defaultPostData.user.photo + i
+    const post = postHTMLGenerate(postImg, userPhoto, isHorizontal)
 
-        posts.push(postsColumn)
+    //  Создаем пост 2
+    const post2Img = defaultPostData.img + i + 1
+    const user2Photo = defaultPostData.user.photo + i + 1
+    const post2 = postHTMLGenerate(post2Img, user2Photo, !isHorizontal)
 
-        isHorizontal = !isHorizontal
-    }
-    return posts
+    // Создаем блок колонку постов
+    const postsColumn = document.createElement('div')
+    postsColumn.appendChild(post)
+    postsColumn.appendChild(post2)
+
+    posts.push(postsColumn)
+
+    isHorizontal = !isHorizontal
+  }
+  return posts
 }
 
 const postsGenerated = postGeneration(50)
@@ -95,5 +97,5 @@ const postsBlock = document.querySelector('.posts')
 postsBlock.innerHTML = ''
 
 postsGenerated.forEach((post) => {
-    postsBlock.appendChild(post)
+  postsBlock.appendChild(post)
 })
